7a2469a52186866523d04376e07263110f3f4d8b
Rocky Durbin <rockydurbin8@gmail.com>
2018-09-14 12:51:58 -0400

    Added button for adding items. Added buttons for crossing off and/or deleting item from list

--- app.js
+++ app.js
@@ -1,65 +1,40 @@
 $(document).ready(function() {
+var items = JSON.parse(localStorage["items"]) || [];
 
+function addToList(val) {
+  $("#pendingItems").append("<li>" + val + "  <button class='purchasedItem'>Purchased</button> <button class='deleteItem'>Delete Item</button></li>");
+}
 
-  $(".add-text-btn").on("click", function(){
+for (var i = 0; i < items.length; i++) {
+  addToList(items[i]);
+}
 
-    // store values
-    let inputKey = $(".user-input-title").val();
-    let inputValue = $(".user-input-body").val();
+var addGoods = function() {
+  var val = $("#item").val();
+  if (val !== "") {
+  items.push(val);
+  localStorage["items"] = JSON.stringify(items);
+  addToList(val, items.length - 1);
+}
+  $("#item").val("").focus();
+}
 
-    // clear values
-    $(".user-input-title").val("");
-    $(".user-input-body").val("");
+$("#addItem").click(addGoods);
+$("#item").keyup(function(event) {
+  if (event.keyCode === 13) {
+    addGoods();
+  }
+});
 
-    console.log(inputKey, inputValue);
+$('#pendingItems').on('click', '.purchasedItem', function() {
+  localStorage["items"] = JSON.stringify(items);
+  $(this).parent('li').addClass('purchased');
+});
 
-    localStorage.setItem(inputKey, inputValue);
-    // data-
-    let itemHtml = '<div class="display-item" data-storage-key="'+inputKey+'"> ' + inputKey + ' ' +  localStorage.getItem(inputKey) + '</div>';
-    $(".display").html(itemHtml);
-    //console.log(localStorage);
-    // how can we delegate this event to the outer html node?
-    // https://learn.jquery.com/events/event-delegation/
-
-    $(".display-item").on("click", function(e){
-      // plop the key:value back into the input boxes
-
-      // get the values from the the divs?
-      console.log("key=> ", e.target.dataset.storageKey); // user-input-title
-      localStorage.getItem(e.target.dataset.storageKey); // user-input-body
-
-      // set those values in the form fields
-      $(".user-input-title").val(e.target.dataset.storageKey);
-      $(".user-input-body").val(localStorage.getItem(e.target.dataset.storageKey));
-    });
-
-  });
-
-
-
-   // TODO add back in later
-   // $(".user-input").on("keyup", function(){
-   //   let inputValue = $(".user-input").val();
-   //   localStorage.setItem("testStorage", inputValue);
-   //   $(".display").text(localStorage.getItem("testStorage"));
-   // });
-
-   $(".del-text-btn").on("click", function() {
-     alert('item deleted? check the console'); // maybe change to a window.confirm
-     localStorage.removeItem( $('.user-input-title').val() ); // grab the title and plop here
-     $(".user-input-title").val("");
-     $(".user-input-body").val("");
-     // clearing display? what if I have multiple items?
-     // after item is removed from local storage, redisplay items from local storage
-     // refresh from storage?
-   });
-
-
-   // iterative approach to adding items
-   // store data as stringified array of objects
-   // store data with individual keys
-  // how do we get keys? research Object.keys
-
-
+$('#pendingItems').on('click', '.deleteItem', function() {
+  items.splice($(this).parent('li').index(), 1);
+  localStorage["items"] = JSON.stringify(items);
+  $(this).parent('li').remove();
+});
 
 });
